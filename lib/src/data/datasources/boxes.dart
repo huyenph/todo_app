7bca9bb8152f8bc5f8a9a0200b733bd7703ec01e
HuyenPham <huyen.phamhoang96@gmail.com>
2022-06-03 10:02:13 +0700

    refactor

--- lib/src/data/datasources/boxes.dart
+++ lib/src/data/datasources/boxes.dart
@@ -1,6 +1,18 @@
+import 'dart:io';
+
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:todo_app/src/domain/model/todo.dart';
 
 class Boxes {
+  static init() async {
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    String appDocPath = appDocDir.path;
+    Hive
+      ..init(appDocPath)
+      ..registerAdapter(TodoAdapter());
+    await Hive.openBox<Todo>('todos');
+  }
+
   static Box<Todo> getTodos() => Hive.box<Todo>('todos');
 }
